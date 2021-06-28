cf30b740578a26492f2a25e40f9c10232de027a0
Minal <patilminalp@gmail.com>
2021-06-28 17:24:51 +0530

    Added:find maximum number of 3 Integer

--- src/com/generic/Maximum.java
+++ src/com/generic/Maximum.java
@@ -3,5 +3,20 @@ package com.generic;
 public class Maximum {
     public static void main(String[] args) {
         System.out.println("Welcome to Generic Program.");
+        Integer a = 2, b = 4, c =6;
+        testMaximum (a,b,c);
     }
+
+    private static void testMaximum(Integer a, Integer b, Integer c) {
+        Integer max = a;
+        if(b.compareTo(a)>0){
+            max = b;
+        }
+        if(c.compareTo(max)>0){
+            max = c;
+        }
+        System.out.println("Maximum Number is::"+max);
+    }
+
+
 }
