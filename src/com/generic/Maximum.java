c18d3ae236373b823a72a528d1da899f56c7c5a1
Minal <patilminalp@gmail.com>
2021-06-28 21:42:35 +0530

    refactor the method using generic

--- src/com/generic/Maximum.java
+++ src/com/generic/Maximum.java
@@ -6,14 +6,15 @@ public class Maximum {
         Integer a = 2, b = 4, c =6;
         Float x = 2.3f, y =7.8f , z = 6.7f;
         String l = "Apple" , m = "Banana" , n = "Orange";
-        testMaximum (a,b,c);
 
-        testMaximum (x,y,z);
-        testMaximum (l,m,n);
+        Maximum.getMaximum(a,b,c);
+        Maximum.getMaximum(x,y,z);
+        Maximum.getMaximum(l,m,n);
     }
 
-    private static void testMaximum(String a, String b, String c) {
-        String max = a;
+    private static <T extends Comparable> void getMaximum(T a, T b, T c) {
+
+        T max = a;
         if(b.compareTo(a)>0){
             max = b;
         }
@@ -21,38 +22,13 @@ public class Maximum {
             max = c;
         }
 
-        System.out.println("Maximum Number of String is::"+max);
-
-
+        System.out.println("Maximum ::"+max);
 
 
     }
 
-    private static void testMaximum(Float a, Float b, Float c) {
-        Float max = a;
-        if(b.compareTo(a)>0){
-            max = b;
-        }
-        if(c.compareTo(max)>0){
-            max = c;
-        }
 
-         System.out.println("Maximum Number of FloatValue is::"+max);
 
-    }
-
-    private static void  testMaximum(Integer a, Integer b, Integer c) {
-        Integer max = a;
-        if(b.compareTo(a)>0){
-            max = b;
-        }
-        if(c.compareTo(max)>0){
-            max = c;
-        }
-
-        System.out.println("Maximum Number of Integer is::"+max);
-
-    }
-
+    
 
 }
