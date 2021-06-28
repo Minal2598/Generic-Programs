d78103ee941761421b4c4c608fb44aaea63eb1ea
Minal <patilminalp@gmail.com>
2021-06-28 19:58:21 +0530

    Added:find max from 3 Strings

--- src/com/generic/Maximum.java
+++ src/com/generic/Maximum.java
@@ -1,29 +1,12 @@
 package com.generic;
 
 public class Maximum {
-    public static void  main(String[] args) {
-        System.out.println("Welcome to Generic Program.");
+    public static void main(String[] args) {
         Integer a = 2, b = 4, c =6;
-        Float x = 2.3f, y =7.8f , z = 6.7f;
-        testMaximum (a,b,c);
-
-        testMaximum (x,y,z);
+        findMaximum (a,b,c);
     }
 
-    private static void testMaximum(Float a, Float b, Float c) {
-        Float max = a;
-        if(b.compareTo(a)>0){
-            max = b;
-        }
-        if(c.compareTo(max)>0){
-            max = c;
-        }
-
-         System.out.println("Maximum Number of FloatValue is::"+max);
-
-    }
-
-    private static void  testMaximum(Integer a, Integer b, Integer c) {
+    private static void findMaximum(Integer a, Integer b, Integer c) {
         Integer max = a;
         if(b.compareTo(a)>0){
             max = b;
@@ -31,9 +14,7 @@ public class Maximum {
         if(c.compareTo(max)>0){
             max = c;
         }
-
-        System.out.println("Maximum Number of Integer is::"+max);
-
+        System.out.println(max);
     }
 
 
