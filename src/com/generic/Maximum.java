707d1a30f7a57f03a8ae2014a91d7175262cad7b
Minal <patilminalp@gmail.com>
2021-06-28 20:10:19 +0530

    Added:find max from 3 Strings

--- src/com/generic/Maximum.java
+++ src/com/generic/Maximum.java
@@ -1,12 +1,47 @@
 package com.generic;
 
 public class Maximum {
-    public static void main(String[] args) {
+    public static void  main(String[] args) {
+        System.out.println("Welcome to Generic Program.");
         Integer a = 2, b = 4, c =6;
-        findMaximum (a,b,c);
+        Float x = 2.3f, y =7.8f , z = 6.7f;
+        String l = "Apple" , m = "Banana" , n = "Orange";
+        testMaximum (a,b,c);
+
+        testMaximum (x,y,z);
+        testMaximum (l,m,n);
     }
 
-    private static void findMaximum(Integer a, Integer b, Integer c) {
+    private static void testMaximum(String a, String b, String c) {
+        String max = a;
+        if(b.compareTo(a)>0){
+            max = b;
+        }
+        if(c.compareTo(max)>0){
+            max = c;
+        }
+
+        System.out.println("Maximum Number of String is::"+max);
+
+
+
+
+    }
+
+    private static void testMaximum(Float a, Float b, Float c) {
+        Float max = a;
+        if(b.compareTo(a)>0){
+            max = b;
+        }
+        if(c.compareTo(max)>0){
+            max = c;
+        }
+
+         System.out.println("Maximum Number of FloatValue is::"+max);
+
+    }
+
+    private static void  testMaximum(Integer a, Integer b, Integer c) {
         Integer max = a;
         if(b.compareTo(a)>0){
             max = b;
@@ -14,7 +49,9 @@ public class Maximum {
         if(c.compareTo(max)>0){
             max = c;
         }
-        System.out.println(max);
+
+        System.out.println("Maximum Number of Integer is::"+max);
+
     }
 
 
